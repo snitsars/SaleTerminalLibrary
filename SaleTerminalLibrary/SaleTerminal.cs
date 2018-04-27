d122f44f782cc2b3c4cea9262bae3e6e6c6628a3
ssnitsarenko@corelogic.com <ssnitsarenko@corelogic.com>
2018-04-28 01:51:10 +0300

    Initial project commit

---
+++ SaleTerminalLibrary/SaleTerminal.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Epam.Demo.SaleTerminalLibrary
+{
+    /// <summary>
+    /// Class of point-of-sale that accepts an arbitrary ordering of products 
+    /// and calculate summary price 
+    /// </summary>
+    public class SaleTerminal
+    {
+        /// <summary>
+        /// Method for set pricing of product 
+        /// prices per unit and volume prices
+        /// </summary>
+        public void SetPricing()
+        {
+            
+        }
+
+
+        /// <summary>
+        /// Scan product method
+        /// </summary>
+        public void Scan(string v)
+        {
+            
+        }
+
+
+        /// <summary>
+        /// Calculate price of all products entire shopping cart 
+        /// </summary>
+        /// <returns></returns>
+        public Double CalculateTotal()
+        {
+            return 0;
+        }
+    }
+}
---
+++ SaleTerminalLibraryTests/SaleTerminalTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Epam.Demo.SaleTerminalLibrary;
+using NUnit.Framework;
+
+namespace Epam.Demo.SaleTerminalLibraryTests
+{
+    [TestFixture]
+    public class SaleTerminalTests
+    {
+        [Test]
+        public void When_ProductSetABCDABA_Expected_TotallPrice13_25()
+        {
+            const double expected = 13.25;
+            SaleTerminal terminal = new SaleTerminal();
+            terminal.SetPricing();
+            terminal.Scan("A");
+            terminal.Scan("B");
+            terminal.Scan("C");
+            terminal.Scan("D");
+            terminal.Scan("A");
+            terminal.Scan("B");
+            terminal.Scan("A");
+            double result = terminal.CalculateTotal();
+            double difference = Math.Abs(result * .00001);
+
+            Assert.That(Math.Abs(result - expected) <= difference);
+        }
+    }
+}
